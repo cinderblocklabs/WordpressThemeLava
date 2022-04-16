abdf5f474fa8aa7f365bc98e08c4a6e1c5aa6b2c
cinderblocklabs <79665029+cinderblocklabs@users.noreply.github.com>
2022-04-16 07:59:52 -0500

    Changed the 404 page
    Nothing major was done in this commit.

--- wp-content/themes/lava/404.php
+++ wp-content/themes/lava/404.php
@@ -1,1 +1,24 @@
 <!-- Responsible for displaying 404 server error -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 ERROR</title>
+</head>
+<body>
+    <div>
+        <header>
+            <h1>ERROR-404</h1>
+            <p>Sorry, we were unable to process your request, please try again some other time.</p>
+        </header>
+    </div>
+    <style>
+        body {
+            background-color:blue;
+            color:white;
+        }
+    </style>
+</body>
+</html>
