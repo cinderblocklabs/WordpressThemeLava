0b6f47257ed13d403c57c648e75ae78716f9fc3f
cinderblocklabs <79665029+cinderblocklabs@users.noreply.github.com>
2022-04-18 15:32:58 -0500

    Working on adding images to all pages.

--- wp-content/themes/lava/index.php
+++ wp-content/themes/lava/index.php
@@ -23,11 +23,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     
-    <!-- FontAwsome -->
+    <!-- FontAwesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
 
     <!-- Custom CSS Stylesheet -->
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen">
 
     <!-- PINYON SCRIPT -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -107,7 +107,8 @@
                 </div>
                 <!-- with 25% height added ('h-25') -->
                 <div class="col-md-5 col-sm-12 h-25">
-                    <img src="assets\images\header-galaxy-image.jpg" alt="No image yet!">
+                    <!-- <img src="header-galaxy-image.jpg" alt="No image yet!"> -->
+                    <img src="header-galaxy-image.jpg" alt="No image yet!">
                 </div>
             </div>
         </div>
@@ -117,7 +118,7 @@
             <container class="text-center">
                 <div class="row">
                     <div class="col-md-6">
-                        <img class="section-1-img" src="assets\images\landscape1.jpg" alt="section 1 image 1">
+                        <img class="section-1-img" src="landscape1.jpg" alt="section 1 image 1">
                         <div class="panel text-right">
                             <h1>Content</h1>
                             <!-- paragraph with top padding of 4px ('pt-4') -->
@@ -126,7 +127,7 @@
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <img class="section-1-img" src="assets\images\landscape2.jpg" alt="section 1 image 2">
+                        <img class="section-1-img" src="landscape2.jpg" alt="section 1 image 2">
                         <div class="panel text-left">
                             <h1>Content</h1>
                             <!-- paragraph with top padding of 4px ('pt-4') -->
@@ -229,21 +230,21 @@
                     <div class="col-md-4">
                         <div class="card">
                             <div class="card-img-top">
-                                <img src="assets/images/face1.jpg" class="img-fluid" alt="card-image">
+                                <img src="face1.jpg" class="img-fluid" alt="card-image">
                             </div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card">
                             <div class="card-img-top">
-                                <img src="assets/face2.jpg" class="img-fluid" alt="card-image">
+                                <img src="face2.jpg" class="img-fluid" alt="card-image">
                             </div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card">
                             <div class="card-img-top">
-                                <img src="assets/images/face3.jpg" class="img-fluid" alt="card-image">
+                                <img src="face3.jpg" class="img-fluid" alt="card-image">
                             </div>
                         </div>
                     </div>
